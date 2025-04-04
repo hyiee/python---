0ca175a77bbd0ce599f164b054860ce42c994a3e
hyiee <summ006@naver.com>
2025-04-04 13:25:41 +0900

    Add files via upload

---
+++ 4.4 과제.py
@@ -0,0 +1,10 @@
+i,k,gugu=0,0,""
+for i in range(2,10):
+    gugu=gugu+("#%d단#"%i)
+print(gugu)
+
+for i in range(1,10):
+    gugu=""
+    for k in range(2,10):
+        gugu=gugu+str("%2dX%2d=%2d"%(k,i,k*i))
+    print(gugu)
